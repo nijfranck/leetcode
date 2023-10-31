f2eb447121c1859c5354b4349c4b011b1d9d8ef5
Franck Nijimbere <nijimbere.franck@gmail.com>
2023-10-31 19:36:09 +0200

    update code

--- 3_Longest_substring_without_repeating_characters.py
+++ 3_Longest_substring_without_repeating_characters.py
@@ -19,3 +19,57 @@ def lengthOfLongestSubstring(s: str) -> int:
         
         return max_counter
 
+# Runtime
+# 395ms
+# Beats 9.64%of users with Python3
+
+# Memory
+# 16.30MB
+# Beats 85.45%of users with Python3
+
+def lengthOfLongestSubstring(s: str) -> int:
+    mydict = {}
+    start = 0
+    max_length = 0
+
+    for end in range(len(s)):
+        if s[end] in mydict:
+            start = max(start, mydict[s[end]] + 1)
+        mydict[s[end]] = end
+        max_length = max(max_length, end - start + 1)
+
+    return max_length
+
+# Runtime
+# 60ms
+# Beats 62.62%of users with Python3
+
+# Memory
+# 16.40MB
+# Beats 54.48%of users with Python3
+
+
+def lengthOfLongestSubstring(s: str) -> int:
+    index = [-1] * 128  # ASCII size
+    start = 0
+    max_length = 0
+
+    for end in range(len(s)):
+        start = max(start, index[ord(s[end])] + 1)
+        index[ord(s[end])] = end
+        max_length = max(max_length, end - start + 1)
+
+    return max_length
+
+
+def lengthOfLongestSubstring(s: str) -> int:
+    index = [-1] * 26  # only lowercase English letters
+    start = 0
+    max_length = 0
+
+    for end in range(len(s)):
+        start = max(start, index[ord(s[end]) - ord('a')] + 1)
+        index[ord(s[end]) - ord('a')] = end
+        max_length = max(max_length, end - start + 1)
+
+    return max_length
